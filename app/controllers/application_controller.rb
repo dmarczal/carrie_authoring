0cc041f657d34b0443d45c34cb6fdb7c0adc605a
Diego Marczal <dmarczal@gmail.com>
2012-04-30 19:34:34 -0300

    changed ckeditor to upload files for a unique user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,4 +15,8 @@ class ApplicationController < ActionController::Base
       authorize! action_name, @asset
     end
   end
+
+  def ckeditor_filebrowser_scope(options = {})
+    super({ :assetable_id => current_user.id, :assetable_type => 'User' }.merge(options))
+  end
 end
